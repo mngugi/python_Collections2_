41d1688c7e55512c33f6cfaab96320fa797847ad
P.M.Ngugi <mngugi7@hotmail.com>
2026-01-15 17:18:24 +0300

    arrays/main.ipynb

---
+++ arrays/.ipynb_checkpoints/arratyCall-checkpoint.py
@@ -0,0 +1,49 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "cb23c97a-bf21-478b-ab0b-e4f762f3a8de",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "def find_min():\n",
+    "    for i in arr:\n",
+    "        if i < smallest_element:\n",
+    "            smallest_element = i\n",
+    "            \n",
+    "    return smallest_element \n",
+    "    "
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "8618c0ac-a257-4f2c-bdd2-f7bb640c1196",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.13.7"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
