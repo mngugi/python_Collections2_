79f36c476d26564a969d441ec71e0e55bdc9e99b
P.M.Ngugi <mngugi7@hotmail.com>
2026-02-17 17:50:45 +0300

    Functions/savings_app/savings_calc.py

---
+++ Functions/savings_app/savings_calc.py
@@ -0,0 +1,9 @@
+
+
+rate = float(input("Enter Interest Rate: "))
+capital =  float(input("Enter Capital: "))
+years = int(input("Enter number of years:"))
+
+def final():
+    total_sum = capital* (1.0+rate)**years
+    return (total_sum)
