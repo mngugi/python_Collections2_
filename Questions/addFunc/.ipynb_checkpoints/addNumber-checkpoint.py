c495039b422798710f361ebb6fab0cc6bf83ed4c
P.M.Ngugi <mngugi7@hotmail.com>
2026-02-19 18:17:24 +0300

    Questions/addFunc/app.ipynb

---
+++ Questions/addFunc/.ipynb_checkpoints/addNumber-checkpoint.py
@@ -0,0 +1,7 @@
+def add(a, b):
+    """Add two numbers and return the result."""
+    return a + b
+
+def add_multiple(*args):
+    """Add multiple numbers and return the result."""
+    return sum(args)
