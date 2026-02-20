d2fa4138074907e13bcf558962c7abca8a5ec422
P.M.Ngugi <mngugi7@hotmail.com>
2026-02-20 22:53:53 +0300

    Functions/calculate_area_func/functions.py

--- Functions/calculate_area_func/functions.py
+++ Functions/calculate_area_func/functions.py
@@ -1,3 +1,8 @@
 def calc_rect_area(length, width):
     return f"Area of a rectangle is: {length * width}"
+
+
+
+def calc_squ_area(length, width):
+    return f'Area of square s: {length,width}'
   
